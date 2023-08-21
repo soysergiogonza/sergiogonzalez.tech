843e56934a487c62a46fdeb75effca3a9a8ae3f6
Sergio González Sánchez <soysergiogonzalez@outlook.es>
2023-08-21 00:01:04 -0500

    fix: little changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
 		<main className="grid justify-center items-center">
-			<h1 className="text-3xl font-bold">This side is in building</h1>
+			<h1 className="text-3xl font-bold">This side is in building!</h1>
 		</main>
 	);
 }

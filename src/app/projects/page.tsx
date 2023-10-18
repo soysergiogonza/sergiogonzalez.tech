e614ffada597c264285ef66ac3120828d5f24c41
Sergio González Sánchez <61368094+SoySergioGonza@users.noreply.github.com>
2023-10-18 00:49:40 -0500

    Card Component (#4)
    * feat: fold card created
    
    * Revert "feat: fold card created"
    
    This reverts commit dcd7ef3daebf03d2162712758b5ffbe097909fbb.
    
    * feat: card component added

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,22 +1,23 @@
-import { Card } from "~/components/Projects/Card/Card";
+import {Card} from '~/components/Projects/Card/Card';
 
 const Projects = () => {
-	return (
-		<div className="grid grid-cols-4 gap-4">
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-		</div>
-	);
+  return (
+    <div
+      className='grid gap-5 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 place-content-center place-items-center grid-cols-4 auto-cols-max'>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
 };
 
 export default Projects;

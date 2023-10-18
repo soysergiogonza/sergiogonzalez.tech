5a07c97cd902fcb5307d9fe2e2f241d67c186d95
Sergio González Sánchez <soysergiogonzalez@outlook.es>
2023-10-18 00:40:01 -0500

    feat: card component added

--- src/components/Projects/Card/Card.tsx
+++ src/components/Projects/Card/Card.tsx
@@ -1,33 +1,30 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 const Card = () => {
-	return (
-		<div className="max-w-md w-full p-4 py-6">
-			<div className="bg-white shadow-xl rounded-lg overflow-hidden">
-				<div className="bg-cover bg-center h-16 p-4 flex justify-end items-center">
-					<p className="uppercase tracking-widest text-sm text-white bg-black py-1 px-2 rounded opacity-75 shadow-lg">DFW <span className="tracking-normal">---</span> SEA</p>
-				</div>
-				<div className="px-4 pb-3 pt-4 border-b border-gray-300 bg-gray-100 flex justify-between">
-					<div className="text-xs uppercase font-bold text-gray-600 tracking-wide">TSA: <span className="font-normal">5-12 min</span></div>
-					<div className="text-xs uppercase font-bold text-gray-600 tracking-wide">Airport Status: <span className="font-normal text-green-600">Normal</span></div>
-				</div>
-				<div className="p-4 text-gray-700 flex justify-between items-start">
-					<div>
-						<p className="text-3xl text-gray-900 leading-none my-1">AA 792</p>
-						<p className="text-xs w-56">American Airlines</p>
-						<p className="text-sm w-56">7:11 am --- 10:10 am</p>
-					</div>
-					<div className="leading-loose bg-green-500 text-white p-1 px-2 rounded-lg uppercase text-xs tracking-wider">On Time</div>
-				</div>
-					<div className='flex gap-4 justify-center p-4 border-t border-gray-300'>
-					<Image src="/react.svg" width={10} height={10} alt='imagen' className="w-10"/>
-					<Image src="/TypeScript.svg" width={30} height={30} alt='imagen' className="w-10"/>
-					<Image src="/jest.svg" width={30} height={30} alt='imagen' className="w-10"/>
-					<Image src="/react-query.svg" width={30} height={30} alt='imagen' className="w-10"/>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className='polygon flex flex-col w-72 rounded-tr-3xl bg-gray-900'>
+      <span className='uppercase bg-gray-500 font-bold text-white text-center'>Elisa Portfolio</span>
+      <div className='w-full'>
+        <Image className='w-full' src='/elisa.png' alt='Cetasdi' width={100} height={100} />
+      </div>
+      <div className='flex flex-col gap-3 px-3 py-3'>
+        <p className='text-start text-white font-normal text-base'>
+          Elisa&apos;s Portfolio is a project that showcases my web development skills and knowledge. I used Flexbox CSS
+          to
+          create an attractive and responsive design.
+        </p>
+        <div className='flex flex-wrap gap-2 justify-start'>
+          <span className='rounded-full border-2 px-2 border-amber-400'>HTML</span>
+          <span className='rounded-full border-2 px-2 border-amber-400'>CSS</span>
+          <span className='rounded-full border-2 px-2 border-amber-400'>JS</span>
+        </div>
+        <div className='flex text-black gap-3 font-bold items-center justify-center'>
+          <button className='px-3 bg-white'>GitHub</button>
+          <button className='px-3 bg-white'>Preview</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export {Card};

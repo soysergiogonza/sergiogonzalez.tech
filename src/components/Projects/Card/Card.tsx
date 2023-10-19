c9d125b724b062c68ba59cf464b2da930497947d
Sergio González Sánchez <soysergiogonzalez@outlook.es>
2023-10-19 12:17:03 -0500

    feat: grid projects features

--- src/components/Projects/Card/Card.tsx
+++ src/components/Projects/Card/Card.tsx
@@ -1,26 +1,45 @@
-import Image from 'next/image';
+'use client';
 
-const Card = () => {
+import Image from 'next/image';
+import {Projects} from '~/data/projects';
+
+interface Props {
+  project: Projects;
+}
+
+const Card = ({project}: Props) => {
+  console.log('project', project.description);
   return (
     <div className='polygon flex flex-col w-72 rounded-tr-3xl bg-gray-900'>
-      <span className='uppercase bg-gray-500 font-bold text-white text-center'>Elisa Portfolio</span>
-      <div className='w-full'>
-        <Image className='w-full' src='/elisa.png' alt='Cetasdi' width={100} height={100} />
+      <span className='uppercase bg-gray-500 font-bold text-white text-center'>{project.name}</span>
+      <div className='w-full h-32 border-2 border-orange-600'>
+        <Image className='object-cover h-full w-full' src={project.image} alt='Cetasdi' width={100}
+               height={100} />
       </div>
       <div className='flex flex-col gap-3 px-3 py-3'>
-        <p className='text-start text-white font-normal text-base'>
-          Elisa&apos;s Portfolio is a project that showcases my web development skills and knowledge. I used Flexbox CSS
-          to
-          create an attractive and responsive design.
+        <p className='text-start text-white h-32 font-normal text-base'>
+          {project.description}
         </p>
         <div className='flex flex-wrap gap-2 justify-start'>
-          <span className='rounded-full border-2 px-2 border-amber-400'>HTML</span>
-          <span className='rounded-full border-2 px-2 border-amber-400'>CSS</span>
-          <span className='rounded-full border-2 px-2 border-amber-400'>JS</span>
+          {
+            project.stack.map((stack: string, index: number) => {
+              return (
+                <span key={index} className='rounded-full border-2 px-2 border-amber-400'>{stack}</span>
+              );
+            })
+          }
         </div>
         <div className='flex text-black gap-3 font-bold items-center justify-center'>
-          <button className='px-3 bg-white'>GitHub</button>
-          <button className='px-3 bg-white'>Preview</button>
+          <button className='px-3 bg-white'>
+            <a href={project.urlGitHub} target='_blank'>GitHub</a>
+          </button>
+          {
+            project.urlPreview.length > 0
+              ? (<button className='px-3 bg-white'>
+                <a href={project.urlPreview} target='_blank'>Preview</a>
+              </button>)
+              : null
+          }
         </div>
       </div>
     </div>

d4ba3e71ccfc8980703f7642f66ad5736d4ba940
Sergio González Sánchez <soysergiogonzalez@outlook.es>
2023-12-26 21:00:09 -0500

    [UPDATE] profile description

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-
 import {DesktopNavigation} from '../DesktopNavigation';
 import {MobileNavigation} from '~/components/Layout/MobileNavigation';
 
--- src/components/sections/Hero/index.tsx
+++ src/components/sections/Hero/index.tsx
@@ -3,21 +3,25 @@
 import Image from 'next/image';
 
 const Hero = () => {
-  return (
-    <section className='hero' aria-label='hero'>
-      <div className='relative rounded-lg shadow-xl sm:gap-4'>
-        <div
-          className='bg-gray-950 z-10 mt-2 w-full flex flex-col md:flex-row items-center gap-4 border-2 rounded-2xl border-b-cyan-50 p-4'>
-          <Image src='/profile.jpeg' width={100} height={100} alt='imagen'
-                 className='border-2 border-white rounded-full' />
-          <div>
-            <h1 className='text-4xl font-bold text-white'>Sergio González Sánchez</h1>
-            <h2 className='text-base font-light text-white'>Frontend Developer | Frontend Architect</h2>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className="hero" aria-label="hero">
+			<div className="relative rounded-lg shadow-xl sm:gap-4">
+				<div
+					className="bg-gray-950 z-10 mt-2 w-full flex flex-col md:flex-row items-center gap-4 border-2 rounded-2xl border-b-cyan-50 p-4">
+					<Image src="/profile.jpeg" width={100} height={100} alt="imagen"
+					       className="border-2 border-white rounded-full"/>
+					<div>
+						<h1 className="text-4xl font-bold text-white">Sergio González Sánchez</h1>
+						<h2 className="text-base font-light text-white">
+							FullStack Developer | Software Architect | Design Patterns | HTML | CSS | JavaScript | TypeScript |
+							Node.js | Express | React.js | Next.js | Redux | Git | GitHub | Azure | Playwright | React Query | Hook
+							Form | SCRUM
+						</h2>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export {Hero};

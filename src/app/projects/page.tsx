0e2ae055e3a6847b5ce13e0943bb0aa2b086f71a
Sergio González Sánchez <61368094+soysergiogonza@users.noreply.github.com>
2024-01-11 10:45:34 -0500

    [FIX] card project component was change his style a little bit (#9)

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,18 +3,18 @@ import {CardProject} from '~/components/Projects/CardProject/CardProject';
 import {Projects} from '~/interfaces/index';
 
 const Projects = () => {
-  return (
-    <div
-      className='grid gap-5 grid-cols-1 place-content-center place-items-center auto-cols-max'>
-      {
-        projects?.map((project: Projects, index: number) => {
-          return (
-            <CardProject key={index} project={project} />
-          );
-        })
-      }
-    </div>
-  );
+	return (
+		<div
+			className="grid gap-5 grid-cols-1 md:grid-cols-2 place-content-center place-items-center auto-cols-max">
+			{
+				projects?.map((project: Projects, index: number) => {
+					return (
+						<CardProject key={index} project={project}/>
+					);
+				})
+			}
+		</div>
+	);
 };
 
 export default Projects;

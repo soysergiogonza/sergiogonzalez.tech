ae3166919b02d348326848adf3b833e5d32c2897
Sergio González Sánchez <soysergiogonzalez@outlook.es>
2024-01-21 22:01:57 -0500

    [STYLE] styles was change from tailwind to vanilla css

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,20 +1,20 @@
 import {projects} from '~/data/projects';
-import {CardProject} from '~/components/Projects/CardProject/CardProject';
 import {Projects} from '~/interfaces/index';
+import {CardProject} from '~/components/Projects';
 
 const Projects = () => {
-	return (
-		<div
-			className="grid gap-5 grid-cols-1 md:grid-cols-2 place-content-center place-items-center auto-cols-max">
-			{
-				projects?.map((project: Projects, index: number) => {
-					return (
-						<CardProject key={index} project={project}/>
-					);
-				})
-			}
-		</div>
-	);
+  return (
+    <div
+      className="grid gap-5 grid-cols-1 md:grid-cols-2 place-content-center place-items-center auto-cols-max">
+      {
+        projects?.map((project: Projects, index: number) => {
+          return (
+            <CardProject key={index} project={project} />
+          );
+        })
+      }
+    </div>
+  );
 };
 
 export default Projects;
--- src/components/Layout/NavItem/index.tsx
+++ src/components/Layout/NavItem/index.tsx
@@ -3,29 +3,27 @@ import {CSSProperties} from 'react';
 import Link from 'next/link';
 import {usePathname} from 'next/navigation';
 
-import {clsx} from 'clsx';
+import styles from './NavItem.module.css';
 
 interface Props {
-	name: string,
-	url: string,
-	style?: CSSProperties;
+  name: string,
+  url: string,
+  style?: CSSProperties;
 }
 
 export const NavItem = ({name, url}: Props) => {
-	const pathname = usePathname() === url;
-	
-	return (
-		<Link href={url}
-		      className={clsx(
-			      'relative block px-6 py-2 transition mb-1 mt-1  rounded-full',
-			      pathname
-				      ? 'text-amber-600 dark:text-amber-50 bg-orange-600 rounded-full '
-				      : 'hover:text-orange-600 dark:hover:text-amber-600'
-		      )}
-		>
-			<button>
-				{name}
-			</button>
-		</Link>
-	);
+  const pathname = usePathname() === url;
+  
+  return (
+    
+    <Link
+      href={url}
+      className={`${styles.link} ${pathname && styles.active}`}
+    >
+      <button>
+        {name}
+      </button>
+    </Link>
+  
+  );
 };

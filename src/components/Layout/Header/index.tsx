f40600d30000e19aaa2df099801ddd6d8c71385a
Sergio González Sánchez <61368094+soysergiogonza@users.noreply.github.com>
2024-01-21 22:20:19 -0500

    [STYLE] styles was change from tailwind to vanilla css (#11)

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,25 +1,24 @@
 'use client';
 
-import {DesktopNavigation} from '../DesktopNavigation';
-import {MobileNavigation} from '~/components/Layout/MobileNavigation';
+import {MobileNavigation, DesktopNavigation} from '~/components/Layout';
 
 const Header = () => {
-	return (
-		<>
-			<header
-				className="pointer-events-none relative z-50 flex flex-col"
-				style={{
-					marginRight: '1rem',
-					marginBlock: '1rem'
-				}}
-			>
-				<div className="flex flex-1 justify-end md:justify-center">
-					<MobileNavigation/>
-					<DesktopNavigation/>
-				</div>
-			</header>
-		</>
-	);
+  return (
+    <>
+      <header
+        className="pointer-events-none relative z-50 flex flex-col"
+        style={{
+          marginRight: '1rem',
+          marginBlock: '1rem',
+        }}
+      >
+        <div className="flex flex-1 justify-end md:justify-center">
+          <MobileNavigation />
+          <DesktopNavigation />
+        </div>
+      </header>
+    </>
+  );
 };
 
 export {Header};
---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,4 @@
+export {DesktopNavigation} from './DesktopNavigation';
+export {Header} from './Header';
+export {MobileNavigation} from './MobileNavigation';
+export {NavItem} from './NavItem';
